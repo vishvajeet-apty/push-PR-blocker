255dce26d88fe0ec9bb3814031e6dcd37c4a11fb
vishvajeet-apty <vishvajeet.singh@apty.io>
2022-10-11 11:43:26 +0530

    removed config var from input

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,6 @@ const branchName = core.getInput('BRANCH_NAME')
 const bucketName = core.getInput('BUCKET_NAME')
 const serviceName = core.getInput('SERVICE_NAME')
 const region = core.getInput('REGION')
-const configPath = core.getInput('CONFIG_PATH')
 const targetBranch = core.getInput('TARGET_Branch')
 const deploy_environment = core.getInput('ENVIRONMENT_NAME')
 
@@ -11,7 +10,6 @@ export {
   branchName,
   bucketName,
   region,
-  configPath,
   targetBranch,
   deploy_environment,
   serviceName
